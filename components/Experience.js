8903ba0c2fcd3d452b632ae4a574dab633e7c587
Sanket Panda <pandasa123@gmail.com>
2019-05-17 23:50:15 -0400

    Safari is a bit broken but eh it's not the end of the world

--- components/Experience.js
+++ components/Experience.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
 import OpenInNewIcon from 'mdi-react/OpenInNewIcon'
+import TextModal from './TextModal'
 
 const Experience = props => {
   return (
     <section className={props.theme ? 'hero is-medium is-dark' : 'hero is-medium'}>
-      <div className="hero-body">
-        <div className="container">
-
-          <Fade delayIn="800" duration={1200} distance="0px">
+      <Fade delayIn="800" duration={1200} distance="0px">
+        <div className="hero-body">
+          <div className="container">
+            {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
             <div className="columns is-size-5">
               <div className="column is-2 is-hidden-mobile"/>
               <div className="column is-2">
@@ -29,7 +30,9 @@ const Experience = props => {
                   <span className="button is-outlined is-rounded is-small is-purple">CLOUD</span>
                 </span>
                 <br/>
-                <span>Software Engineer</span>
+                <span>Software Engineer </span>
+                <TextModal id="ibm" title={'Software Engineer @ IBM Extreme Blue'}
+                           content={['Extending Salesforce Active Chat and integrating IBM Watson features to expedite average case handling time', 'Built a companion application to monitor support agent availability and facilitate efficient queue handling']}/>
                 <div className="is-hidden-tablet">
                   <span>May 2019 - Present</span>
                 </div>
@@ -38,9 +41,9 @@ const Experience = props => {
                 <span>May 2019 - Present</span>
               </div>
             </div>
-          </Fade>
+            {/*</Fade>*/}
 
-          <Fade delayIn="800" duration={1200} distance="0px">
+            {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -50,7 +53,9 @@ const Experience = props => {
                   <span className="button is-outlined is-rounded is-small is-purple">CLOUD</span>
                 </span>
                 <br/>
-                <span>Volunteer Assistant CS Teacher</span>
+                <span>Volunteer Assistant CS Teacher </span>
+                <TextModal id="citizen" title={'Volunteer Assistant CS Teacher @ Citizen Schools'}
+                           content={['Taught students about scalable software solutions and industry best practices', 'Helped individual students on a per-student basis']}/>
                 <div className="is-hidden-tablet">
                   <span>May 2019 - May 2019</span>
                 </div>
@@ -59,10 +64,10 @@ const Experience = props => {
                 <span>May 2019 - May 2019</span>
               </div>
             </div>
-          </Fade>
+            {/*</Fade>*/}
 
 
-          <Fade delayIn="800" duration={1200} distance="0px">
+            {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -71,7 +76,9 @@ const Experience = props => {
                  <span className="button is-outlined is-rounded is-small is-success">FRONTEND</span>
                 </span>
                 <br/>
-                <span>Research Assistant</span>
+                <span>Research Assistant </span>
+                <TextModal id="umich" title={'Research Assistant @ University of Michigan'}
+                           content={['Worked with Professor Clive D’Souza to build responsive mobility lab data visualisations using D3.js and MySQL', 'Implemented multi-variate models using LALOLib and ML.js']}/>
                 <div className="is-hidden-tablet">
                   <span>January 2019 - April 2019</span>
                 </div>
@@ -80,9 +87,9 @@ const Experience = props => {
                 <span>January 2019 - April 2019</span>
               </div>
             </div>
-          </Fade>
+            {/*</Fade>*/}
 
-          <Fade delayIn="800" duration={1200} distance="0px">
+            {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -93,7 +100,9 @@ const Experience = props => {
                   <span className="button is-outlined is-rounded is-small is-danger">BACKEND</span>
                 </span>
                 <br/>
-                <span>Software Engineer</span>
+                <span>Software Engineer </span>
+                <TextModal id="diesel" title={'Software Engineer @ Diesel Labs'}
+                           content={['Built a visualization that demonstrated Diesel Lab’s data intelligence using React / Redux / Scala Finatra', 'Saved 150+ hours by building an Excel data export API for clients using APACHE POI / React / Redux', 'Delivered onboarding process for new clients using React / Redux']}/>
                 <div className="is-hidden-tablet">
                   <span>May 2018 - August 2018</span>
                 </div>
@@ -102,10 +111,10 @@ const Experience = props => {
                 <span>May 2018 - August 2018</span>
               </div>
             </div>
-          </Fade>
+            {/*</Fade>*/}
 
 
-          <Fade delayIn="800" duration={1200} distance="0px">
+            {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -115,7 +124,9 @@ const Experience = props => {
                   <span className="button is-outlined is-rounded is-small is-danger">BACKEND</span>
                 </span>
                 <br/>
-                <span>Software Engineer</span>
+                <span>Software Engineer </span>
+                <TextModal id="ramp" title={'Software Engineer @ Ramp.Digital'}
+                           content={['Designed a NodeJS / MQTT load balancing application to test various IoT solutions', 'Built a scalable dashboard to monitor and manage IoT solutions']}/>
                 <div className="is-hidden-tablet">
                   <span>June 2017 - September 2017</span>
                 </div>
@@ -124,10 +135,10 @@ const Experience = props => {
                 <span>June 2017 - September 2017</span>
               </div>
             </div>
-          </Fade>
+            {/*</Fade>*/}
 
 
-          <Fade delayIn="800" duration={1200} distance="0px">
+            {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -136,7 +147,10 @@ const Experience = props => {
                   <span className="button is-outlined is-rounded is-small is-danger">BACKEND</span>
                 </span>
                 <br/>
-                <span>Founder</span>
+                <span>Founder </span>
+                <TextModal id="sonorous" title={'Founder @ Sonorous'}
+                           content={['Designed a platform agnostic portable device to wirelessly record acoustic guitars with high fidelity', 'Secured venture capital funding from Kalinga Ventures Startup Competition and the University of Michigan: Center for\n' +
+                           'Entrepreneurship']}/>
                 <div className="is-hidden-tablet">
                   <span>May 2016 - June 2017</span>
                 </div>
@@ -145,10 +159,11 @@ const Experience = props => {
                 <span>May 2016 - June 2017</span>
               </div>
             </div>
-          </Fade>
+            {/*</Fade>*/}
 
+          </div>
         </div>
-      </div>
+      </Fade>
     </section>
   )
 }
---
+++ components/TextModal.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import OpenInNewIcon from 'mdi-react/OpenInNewIcon'
+
+const TextModal = props => {
+  const openModal = () => {
+    document.getElementById(props.id).classList.add('is-active')
+  }
+
+  const closeModal = () => {
+    document.getElementById(props.id).classList.remove('is-active')
+  }
+
+  // style={{position: 'fixed', top: 0, bottom: 0}}
+
+  return (
+    <span>
+      <a className="icon has-text-info" onClick={openModal} style={{position: 'relative', top: '2px'}}>
+        <OpenInNewIcon size={'1rem'}/>
+      </a>
+      <br/>
+      <div className="modal is-clipped" id={props.id}>
+        <div onClick={closeModal} className="modal-background"
+             style={{top: '0', bottom: '0', left: '0', right: '0', position: 'absolute'}}/>
+        <div className="modal-content">
+          <article className="message is-info is-medium">
+          <div className="message-header">
+            <p>{props.title}</p>
+            {/*<button className="delete" aria-label="delete" onClick={closeModal}/>*/}
+          </div>
+          <div className="message-body">
+            <ul className="is-centered">
+              {props.content.map(d => {
+                return (<li key={d[0]} style={{paddingBottom: '10px'}}>{d}</li>)
+              })}
+            </ul>
+          </div>
+        </article>
+        </div>
+      </div>
+    </span>
+  )
+}
+
+export default TextModal
+
