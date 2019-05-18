de16128515f9d1e9e889f64b184feaf39d574431
Sanket Panda <pandasa123@gmail.com>
2019-05-18 06:36:54 -0400

    Just plain visible for now. Will animate later

--- components/Experience.js
+++ components/Experience.js
@@ -6,9 +6,9 @@ const Experience = props => {
   return (
     <section className={props.theme ? 'hero is-medium is-dark' : 'hero is-medium'}>
       {/*<Fade delayIn="800" duration={1200} distance="0px">*/}
-        <div className="hero-body">
-          <div className="container">
-            <Fade delayIn="800" duration={1200} distance="0px">
+      <div className="hero-body">
+        <div className="container">
+          <Fade delayIn="800" duration={1200} distance="0px">
             <div className="columns is-size-5">
               <div className="column is-2 is-hidden-mobile"/>
               <div className="column is-2">
@@ -29,20 +29,29 @@ const Experience = props => {
                   <span className="button is-outlined is-rounded is-small is-purple">CLOUD</span>
                 </span>
                 <br/>
-                <span>Software Engineer </span>
-                {/*<TextModal id="ibm" title={'Software Engineer @ IBM Extreme Blue'}*/}
-                {/*           content={['Extending Salesforce Active Chat and integrating IBM Watson features to expedite average case handling time', 'Built a companion application to monitor support agent availability and facilitate efficient queue handling']}/>*/}
+                <span>Software Engineer</span>
                 <div className="is-hidden-tablet">
                   <span>May 2019 - Present</span>
                 </div>
+                <ul className="is-size-7">
+                  <br/>
+                  <li>Extending Salesforce Active Chat and integrating IBM Watson features to expedite average case
+                    handling time
+                  </li>
+                  <br/>
+                  <li>Built a companion application to monitor support agent availability and facilitate efficient
+                    queue
+                    handling
+                  </li>
+                </ul>
               </div>
               <div className="column is-hidden-mobile">
                 <span>May 2019 - Present</span>
               </div>
             </div>
-            </Fade>
+          </Fade>
 
-            <Fade delayIn="800" duration={1200} distance="0px">
+          <Fade delayIn="800" duration={1200} distance="0px">
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -53,20 +62,26 @@ const Experience = props => {
                 </span>
                 <br/>
                 <span>Volunteer Assistant CS Teacher </span>
-                {/*<TextModal id="citizen" title={'Volunteer Assistant CS Teacher @ Citizen Schools'}*/}
-                {/*           content={['Taught students about scalable software solutions and industry best practices', 'Helped individual students on a per-student basis']}/>*/}
                 <div className="is-hidden-tablet">
                   <span>May 2019 - May 2019</span>
                 </div>
+                <ul className="is-size-7">
+                  <br/>
+                  <li>Taught students about scalable software solutions and industry best practices
+                  </li>
+                  <br/>
+                  <li>Helped individual students on a per-student basis
+                  </li>
+                </ul>
               </div>
               <div className="column is-hidden-mobile">
                 <span>May 2019 - May 2019</span>
               </div>
             </div>
-            </Fade>
+          </Fade>
 
 
-            <Fade delayIn="800" duration={1200} distance="0px">
+          <Fade delayIn="800" duration={1200} distance="0px">
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -76,19 +91,26 @@ const Experience = props => {
                 </span>
                 <br/>
                 <span>Research Assistant </span>
-                {/*<TextModal id="umich" title={'Research Assistant @ University of Michigan'}*/}
-                {/*           content={['Worked with Professor Clive D’Souza to build responsive mobility lab data visualisations using D3.js and MySQL', 'Implemented multi-variate models using LALOLib and ML.js']}/>*/}
                 <div className="is-hidden-tablet">
                   <span>January 2019 - April 2019</span>
                 </div>
+                <ul className="is-size-7">
+                  <br/>
+                  <li>Worked with Professor Clive D’Souza to build responsive mobility lab data visualisations using
+                    D3.js and MySQL
+                  </li>
+                  <br/>
+                  <li>Implemented multi-variate models using LALOLib and ML.js
+                  </li>
+                </ul>
               </div>
               <div className="column is-hidden-mobile">
                 <span>January 2019 - April 2019</span>
               </div>
             </div>
-            </Fade>
+          </Fade>
 
-            <Fade delayIn="800" duration={1200} distance="0px">
+          <Fade delayIn="800" duration={1200} distance="0px">
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -100,20 +122,30 @@ const Experience = props => {
                 </span>
                 <br/>
                 <span>Software Engineer </span>
-                {/*<TextModal id="diesel" title={'Software Engineer @ Diesel Labs'}*/}
-                {/*           content={['Built a visualization that demonstrated Diesel Lab’s data intelligence using React / Redux / Scala Finatra', 'Saved 150+ hours by building an Excel data export API for clients using APACHE POI / React / Redux', 'Delivered onboarding process for new clients using React / Redux']}/>*/}
                 <div className="is-hidden-tablet">
                   <span>May 2018 - August 2018</span>
                 </div>
+                <ul className="is-size-7">
+                  <br/>
+                  <li>Built a visualization that demonstrated Diesel Lab’s data intelligence using React / Redux / Scala
+                    Finatra
+                  </li>
+                  <br/>
+                  <li>Saved 150+ hours by building an Excel data export API for clients using APACHE POI / React / Redux
+                  </li>
+                  <br/>
+                  <li>Delivered onboarding process for new clients using React / Redux
+                  </li>
+                </ul>
               </div>
               <div className="column is-hidden-mobile">
                 <span>May 2018 - August 2018</span>
               </div>
             </div>
-            </Fade>
+          </Fade>
 
 
-            <Fade delayIn="800" duration={1200} distance="0px">
+          <Fade delayIn="800" duration={1200} distance="0px">
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -125,19 +157,27 @@ const Experience = props => {
                 <br/>
                 <span>Software Engineer </span>
                 {/*<TextModal id="ramp" title={'Software Engineer @ Ramp.Digital'}*/}
-                {/*           content={['Designed a NodeJS / MQTT load balancing application to test various IoT solutions', 'Built a scalable dashboard to monitor and manage IoT solutions']}/>*/}
+                {/*           content={['', '']}/>*/}
                 <div className="is-hidden-tablet">
                   <span>June 2017 - September 2017</span>
                 </div>
+                <ul className="is-size-7">
+                  <br/>
+                  <li>Designed a NodeJS / MQTT load balancing application to test various IoT solutions
+                  </li>
+                  <br/>
+                  <li>Built a scalable dashboard to monitor and manage IoT solutions
+                  </li>
+                </ul>
               </div>
               <div className="column is-hidden-mobile">
                 <span>June 2017 - September 2017</span>
               </div>
             </div>
-            </Fade>
+          </Fade>
 
 
-            <Fade delayIn="800" duration={1200} distance="0px">
+          <Fade delayIn="800" duration={1200} distance="0px">
             <div className="columns is-size-5">
               <div className="column is-4 is-hidden-mobile"/>
               <div className="column">
@@ -147,21 +187,28 @@ const Experience = props => {
                 </span>
                 <br/>
                 <span>Founder </span>
-                {/*<TextModal id="sonorous" title={'Founder @ Sonorous'}*/}
-                {/*           content={['Designed a platform agnostic portable device to wirelessly record acoustic guitars with high fidelity', 'Secured venture capital funding from Kalinga Ventures Startup Competition and the University of Michigan: Center for\n' +*/}
-                {/*           'Entrepreneurship']}/>*/}
                 <div className="is-hidden-tablet">
                   <span>May 2016 - June 2017</span>
                 </div>
+                <ul className="is-size-7">
+                  <br/>
+                  <li>Designed a platform agnostic portable device to wirelessly record acoustic guitars with high
+                    fidelity
+                  </li>
+                  <br/>
+                  <li>Secured venture capital funding from Kalinga Ventures Startup Competition and the University of
+                    Michigan: Center for Entrepreneurship
+                  </li>
+                </ul>
               </div>
               <div className="column is-hidden-mobile">
                 <span>May 2016 - June 2017</span>
               </div>
             </div>
-            </Fade>
+          </Fade>
 
-          </div>
         </div>
+      </div>
       {/*</Fade>*/}
     </section>
   )
