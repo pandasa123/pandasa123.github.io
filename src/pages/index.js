c63bef1bd528c8fe3a58978b0b9d4ca33bff3d9b
Sanket Panda <pandasa123@gmail.com>
2019-07-01 17:52:38 -0500

    Redesigned. Time to start again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,14 @@
 import React from 'react'
 // import Layout from '../components/layout'
 import SEO from '../components/seo'
-import HeroHeader from '../components/landing_page/HeroHeader'
-import HeroNavigation from '../components/landing_page/HeroNavigation'
-import Hermes from '../components/landing_page/Hermes'
-import Ergonomics from '../components/landing_page/Ergonomics'
 
 const IndexPage = () => {
     return (
-        <div className="h-full w-full">
+        <div>
             <SEO
                 title="Home"
                 keywords={['gatsby', 'tailwind', 'react', 'tailwindcss']}
             />
-            <HeroHeader/>
-            <HeroNavigation/>
-            <Hermes/>
-            <Ergonomics/>
         </div>
     )
 }
