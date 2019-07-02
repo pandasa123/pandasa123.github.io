e744d145cd71d982161d71a1df38811c06bebf8d
Sanket Panda <pandasa123@gmail.com>
2019-07-02 17:43:17 -0500

    Padding both sides of footer for consistency

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<footer className="bg-blue-500">
-			<div className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
+			<div className="flex px-6 justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
 				<p className="text-white">Created by Sanket Panda</p>
 				<p>
 					<span className="font-bold no-underline text-white">
