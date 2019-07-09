a315a311221dfc3d57b299db2cc306effbff969f
Sanket Panda <pandasa123@gmail.com>
2019-07-08 20:53:45 -0500

    Added TypeScript. About to convert

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+// See https://next.tailwindcss.com/docs/configuration for details
+
+module.exports = {
+	theme: {
+		extend: {
+			spacing: {
+				9: '2.25rem',
+				28: '7rem',
+			},
+			maxWidth: {
+				'7xl': '80rem',
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+}
