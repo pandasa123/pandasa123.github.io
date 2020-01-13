47066cb955d1cbb429d88180d58a4b6832b366ed
Sanket Panda <pandasa123@gmail.com>
2020-01-13 00:32:49 -0500

    Added underline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { CardStack } from './Components/CardStack/'
+import './App.css'
 
 const App = () => {
-	return <CardStack />
+	return (
+		<>
+			<div id="header">
+				<h1 className="page_title">
+					<span className="underline--magical">Hi, I'm Sanket Panda</span>
+				</h1>
+			</div>
+			<div id="card_stack_container">
+				<CardStack />
+			</div>
+		</>
+	)
 }
 
 export default App
